21fa990572e49a1a043d096fe761d43ee7a0e94a
Vivian Nnolum <viviannnolum23@gmail.com>
2025-10-02 11:12:50 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,252 @@
+// ------BEGINNING------
+
+
+// import Header from "./header.jsx"
+// import Footer from "./footer.jsx"
+// import Food from "./food.jsx"
+
+// function App(){
+//   return(
+//     <>
+//       <Header/>
+//       <Food/>
+//       <Footer/>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+// ------CARD-----
+
+// import Card from "./card.jsx"
+
+// function App(){
+//   return(
+//     <>
+//       <Card/>
+//       <Card/>
+//       <Card/>
+//       <Card/>
+//     </>
+//   );
+// }
+
+// export default App
+
+
+
+// ------EXTERNAL, MODULUS(external folfer entirely) & INLINE(involves const...) CSS STYLING----
+
+// import Button  from "./button.jsx"
+
+// function App(){
+//   return(
+//     <Button/>
+//   )
+// }
+
+// export default App
+
+
+
+// -----PROPS-----
+
+// import Student from "./student.jsx";
+
+// function App(){
+//   return(
+//     <>
+//       <Student name="Spongebob" age={30} isStudent={true}/>
+//       <Student name="Patrick" age={60} isStudent={false}/>
+//       <Student name="Squidward" age={20} isStudent={true}/>
+//       <Student name="Sandy" age={10} isStudent={false}/>
+//       <Student/>
+//     </>
+//   );
+// }
+
+// export default App
+
+
+
+// -----CONDITIONAL RENDERING-----
+
+
+
+// import UserGreeting from "./userGreeting.jsx";
+
+// function App(){
+//   return(
+//     <>
+//       <UserGreeting isLoggedIn ={true} username ="Vivian"/>
+    
+//     </>
+//   );
+// }
+
+// export default App
+
+
+// ------RENDERING LISTS------
+
+
+
+// import List from "./list.jsx";
+
+// function App(){
+//   return(
+//     <>
+//       <List category ="Fruits"/>
+      {/* <List items= {listItem} category ="Vegetables"/>  */}
+// 
+
+
+
+// -----CLICK EVENTS-----
+
+
+// import Buttons from "./buttons.jsx";
+
+// function App(){
+
+//   return(
+//       <Buttons/>
+//   );
+// }
+
+// export default App
+
+// import ProfilePic from "./profilePic.jsx";
+
+// function App(){
+//   return(
+//     <>
+//        <ProfilePic/>
+//     </>
+//   );
+// }
+
+// export default App
+
+
+// -----REACT HOOK-----
+
+// import MyComponent from "./myComponent.jsx";
+
+// function App(params) {
+//   return(
+//     <>
+//       <MyComponent/>
+//     </>
+//   );
+// }
+
+// export default App
+
+
+// import Counter from "./counter.jsx";
+
+// function App(){
+//   return(
+//       <Counter/>
+//   );
+// }
+
+// export default App
+
+
+// ---ONCHANGE----
+
+
+// import MyComponents from "./myComponents.jsx";
+
+// function App(){
+//   return(
+//       <MyComponents/>
+//   );
+// }
+
+// export default App
+
+
+// -----UPDATER FUNCTION----- used for increment, decrement and reset (c=>c+1)
+
+
+// import ToDoList from "./toDoList.jsx" 
+
+// function App(){
+//   return(
+//     <>
+//       <ToDoList/>
+//     </>
+//   );
+// }
+
+// export default App
+
+
+
+// -----USEEFFECT REACT HOOK------
+
+
+// import UseEffect from "./useEffect.jsx";
+
+// function App(){
+//   return(
+//     <>
+//       <UseEffect/>
+//     </>
+//   );
+// }
+
+// export default App
+
+
+// import DigitalClock from "./digitalclock.jsx"
+
+// function App(){
+//   return(
+//     <>
+//       <DigitalClock/>
+//     </>
+//   );
+// }
+
+// export default App
+
+
+// ------USECONTEXT-----
+
+
+
+// import ComponentA from "./componentA.jsx";
+// import React from "react";
+
+// function App(){
+//   return(
+//     <>
+//       <ComponentA/>
+//     </>
+//   );
+// }
+
+// export default App
+
+
+
+// ------USEREF-----
+
+
+
+import Stopwatch from "./stopwatch.jsx";
+
+function App(){
+  return(
+      <Stopwatch/>
+  );
+}
+
+export default App;
